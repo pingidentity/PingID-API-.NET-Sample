acac16115a4af1b3a0cc8e7e3959c01fcd425e82
Paul Meyer <pmeyer@pingidentity.com>
2015-10-14 14:45:53 -0600

    Initial commit

---
+++ PingID-API-.NET-Sample/PingID-API-.NET-Sample/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace PingID_API_.NET_Sample
+{
+    class Program
+    {
+        //Update with your PingID settings:
+        public static string org_alias      = "<< org_alias value from pingid.properties file >>";
+        public static string use_base64_key = "<< use_base64_key value from pingid.properties file >>";
+        public static string token          = "<< token value from pingid.properties file >>";
+        public static string api_version    = "4.6";
+
+        public static string Base64UrlEncodeString(string rawString)
+        {
+            return Base64UrlEncodeString(Encoding.UTF8.GetBytes(rawString));
+        }
+
+        public static string Base64UrlEncodeString(byte[] rawBytes)
+        {
+            var encodedString = Convert.ToBase64String(rawBytes);
+
+            encodedString = encodedString.Replace('+', '-');
+            encodedString = encodedString.Replace('/', '_');
+            encodedString = encodedString.TrimEnd(new char[] { '=' });
+
+            return encodedString;
+        }
+
+        public static byte[] Base64UrlDecodeString(string encodedString)
+        {
+            encodedString = encodedString.Replace('-', '+');
+            encodedString = encodedString.Replace('_', '/');
+            encodedString = encodedString.PadRight(encodedString.Length + (4 - encodedString.Length % 4) % 4, '=');
+
+            return Convert.FromBase64String(encodedString);
+        }
+
+        public static string DictionaryToJsonString(Dictionary<string, object> dictionary)
+        {
+            JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
+            return jsonSerializer.Serialize(dictionary);
+        }
+
+        public static string sendToken(Dictionary<string, object> requestBody, string apiEndpoint)
+        {
+            Dictionary<string, object> jwtHeader = new Dictionary<string, object>();
+            jwtHeader.Add("alg", "HS256");
+            jwtHeader.Add("org_alias", org_alias);
+            jwtHeader.Add("token", token);
+
+            var headerSerialized = DictionaryToJsonString(jwtHeader);
+            var headerEncoded = Base64UrlEncodeString(headerSerialized);
+
+            Dictionary<string, object> reqHeaderClaims = new Dictionary<string, object>();
+            reqHeaderClaims.Add("locale", "en");
+            reqHeaderClaims.Add("orgAlias", org_alias);
+            reqHeaderClaims.Add("secretKey", token);
+            reqHeaderClaims.Add("timestamp", DateTime.UtcNow);
+            reqHeaderClaims.Add("version", api_version);
+
+            Dictionary<string, object> jwtPayload = new Dictionary<string, object>();
+            jwtPayload.Add("reqHeader", reqHeaderClaims);
+            jwtPayload.Add("reqBody", requestBody);
+
+            var payloadSerialized = DictionaryToJsonString(jwtPayload);
+            var payloadEncoded = Base64UrlEncodeString(payloadSerialized);
+
+            var signedComponents = String.Join(".", headerEncoded, payloadEncoded);
+
+            var HMAC = new HMACSHA256(Base64UrlDecodeString(use_base64_key));
+            var signatureBytes = HMAC.ComputeHash(Encoding.UTF8.GetBytes(signedComponents));
+            var signatureEncoded = Base64UrlEncodeString(signatureBytes);
+
+            var apiToken = String.Join(".", signedComponents, signatureEncoded);
+
+            // Send the JWS
+            var response = HttpPost(apiEndpoint, apiToken);
+
+            return response;
+        }
+
+        public static string HttpPost(string URI, string jwt)
+        {
+            WebRequest webRequest = WebRequest.Create(URI);
+
+            webRequest.ContentType = "application/json";
+            webRequest.Method = "POST";
+            byte[] payload = Encoding.UTF8.GetBytes(jwt);
+            webRequest.ContentLength = payload.Length;
+
+            Stream outputStream = webRequest.GetRequestStream();
+            outputStream.Write(payload, 0, payload.Length);
+            outputStream.Close();
+
+            WebResponse webResponse = webRequest.GetResponse();
+            if (webResponse == null) return null;
+
+            StreamReader sr = new StreamReader(webResponse.GetResponseStream());
+            return sr.ReadToEnd().Trim();
+        }
+
+        static void Main(string[] args)
+        {
+            // Example call to the GetUserDetails operation
+
+            string userName = "meredith";
+
+            Dictionary<string, object> reqBody = new Dictionary<string, object>();
+            reqBody.Add("userName", userName);
+            reqBody.Add("getSameDeviceUsers", false);
+
+            var apiEndpoint = "https://idpxnyl3m.pingidentity.com/pingid/rest/4/getuserdetails/do";
+
+            string apiResponse = sendToken(reqBody, apiEndpoint);
+
+            string[] responseComponents = apiResponse.Split(new char[] { '.' });
+
+            string responsePayload = responseComponents[1];
+            string responsePayloadDecoded = Encoding.UTF8.GetString(Base64UrlDecodeString(responsePayload));
+
+            Console.WriteLine("Payload: ");
+            Console.WriteLine(responsePayloadDecoded);
+
+            Console.WriteLine("Press a key to continue... ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+#pingid-api-.NET-sample
+
+### Overview
+
+Sample C# .NET example of using the PingID API. This example performs a GetUserDetails operation and displays the parsed response.
+
+### System Requirements / Dependencies
+
+Requires:
+ - PingOne account with PingID service enabled (visit [Ping Identity Developer Site] to get a developer account)
+ 
+### Installation
+ 
+1. Import the project into Visual Studio
+2. Update the source code to include your PingID configuration settings (from your pingid.properties file)
+3. Update the main method to set the username you wish to retrieve
+4. Launch the console application
+
+### Disclaimer
+
+This software is open sourced by Ping Identity but not supported commercially as such. Any questions/issues should go to the Github issues tracker or discuss on the [Ping Identity developer communities] . See also the DISCLAIMER file in this directory.
+
+[Ping Identity developer communities]: https://community.pingidentity.com/collaborate
+[Ping Identity Developer Site]: https://developer.pingidentity.com/connect
